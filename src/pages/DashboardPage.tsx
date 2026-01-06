be27a1da5c46b4cd5f2a84a7be107069350eb629
Erfan irandoust <erfanirandoost.1997@gmail.com>
2026-01-06 15:58:25 +0330

    init

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -6,6 +6,7 @@ import { MotionSelect, ValueBadgeDisplay } from '../components';
 import type { DataGridColumn } from '../components/tables/DataTable/DataTable.types';
 import ResponsiveDataGrid from '../components/tables/DataTable/ResponsiveDataTable';
 import { StatusBadge } from '../components/ui/badges/StatusBadge';
+import { ChevronLeft } from 'lucide-react';
 
 // ======================================================================================================|
 interface DataType {
@@ -95,7 +96,7 @@ export default function DashboardPage() {
   return (
     <div className="px-4 pb-4">
       <p className="text-lg font-bold mb-5">میزکار من</p>
-      <div className="border border-global-3 rounded-2xl p-2">
+      <div className="border border-global-3 rounded-2xl p-2 mb-6">
         <div className="flex items-center justify-between mb-3">
           <div className="flex items-center gap-x-6 w-full">
             <ValueBadgeDisplay label="مجموع درآمد" number={1} unit />
@@ -114,6 +115,28 @@ export default function DashboardPage() {
           />
         </div>
         <ResponsiveDataGrid<DataType> data={data} columns={dataColumns} />
+        <div className="flex items-center justify-center gap-x-4 mt-3">
+          <button
+            type="button"
+            className="bg-accent/40 p-1.5 rounded-lg text-white"
+          >
+            <ChevronLeft size={18} className="rotate-180" />{' '}
+          </button>
+          <button
+            type="button"
+            className="bg-global-5 p-1.5 rounded-lg text-white"
+          >
+            <ChevronLeft size={18} />{' '}
+          </button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-12 gap-x-6">
+        <div className="col-span-12 lg:col-span-4 border border-global-3 rounded-2xl p-7">
+          <p className="text-base mb-3">امتیاز شما در کلینیک حقوقی</p>
+          <p className=''>امتیاز فعلی شما برای ورود به رتبه‌بندی کافی نیست.</p>
+        </div>
+        <div className="col-span-12 lg:col-span-8 border border-global-3 rounded-2xl p-7"></div>
       </div>
     </div>
   );
