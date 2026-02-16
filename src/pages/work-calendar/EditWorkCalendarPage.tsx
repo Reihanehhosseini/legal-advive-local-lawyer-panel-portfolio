dbc415263bce74dede2447c253f74ff45aab310e
Erfan irandoust <erfanirandoost.1997@gmail.com>
2026-02-16 16:01:25 +0330

    add action buttons for edit work calendar page

--- src/pages/work-calendar/EditWorkCalendarPage.tsx
+++ src/pages/work-calendar/EditWorkCalendarPage.tsx
@@ -34,7 +34,7 @@ export default function EditWorkCalendarPage() {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-x-5 gap-y-8">
-            <div className="col-span-2 lg:col-span-2 flex flex-col gap-y-2">
+            <div className="col-span-2 lg:col-span-1 flex flex-col gap-y-2">
               <span className="bg-primary text-white w-fit px-3 py-0.5 rounded-full">
                 شنبه
               </span>
@@ -62,6 +62,48 @@ export default function EditWorkCalendarPage() {
                 <span>تعطیل</span>
               </div>
             </div>
+            <div className="col-span-2 lg:col-span-1 flex flex-col gap-y-2">
+              <span className="bg-primary text-white w-fit px-3 py-0.5 rounded-full">
+                یکشنبه
+              </span>
+
+              <div className="flex items-center gap-x-3">
+                از
+                <MotionSelect
+                  options={[{ id: 1, key: 's', value: '2' }]}
+                  onSelect={(val) => console.log(val)}
+                />
+                تا
+                <MotionSelect
+                  options={[{ id: 1, key: 's', value: '2' }]}
+                  onSelect={(val) => console.log(val)}
+                />
+                <button
+                  type="button"
+                  className="min-w-10.5 h-10.5 rounded-2xl bg-global-5 flex items-center justify-center"
+                >
+                  <HiOutlinePlusSm size={22} />
+                </button>
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input type="checkbox" />
+                <span>تعطیل</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-end gap-y-4 gap-x-6">
+            <button
+              type="button"
+              className="bg-global-5 text-white rounded-full w-full max-w-96 py-2.5"
+            >
+              لغو
+            </button>
+            <button
+              type="button"
+              className="bg-primary text-white rounded-full w-full max-w-96 py-2.5"
+            >
+              بروزرسانی
+            </button>
           </div>
         </Form>
       </Formik>
