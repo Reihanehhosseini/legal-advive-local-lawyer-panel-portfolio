f9cc5815ee50612c4ba2543f8f264d3342b8a3d7
reihaneh hosseini <reihaneh_h1368@yahoo.com>
2026-06-06 15:29:28 +0330

    base fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"legal-advive-local-lawyer-panel-portfolio",
+  base:"/legal-advive-local-lawyer-panel-portfolio/",
   plugins: [react(), tailwindcss()],
 
 });
